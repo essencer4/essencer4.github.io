79a0b919246ffd7fc149efe676e499e8081f7077
Dmitriy Svetov <essencer4@mail.ru>
2016-10-15 09:28:07 +0400

    1.7.9
    1.7.9

--- js/script.js
+++ js/script.js
@@ -1,53 +1,11 @@
-var link = document.querySelector(".js-button");
-		
-var popup = document.querySelector(".modal-content");
-		
-var close = popup.querySelector(".modal-content-close");
-		
-link.addEventListener("click", function(event) {
-		
-	event.preventDefault();
-	popup.classList.add("modal-content-show");
+jQuery(function(f){
+
+    var element = f('#blfix');
+
+    f(window).scroll(function(){
+
+        element['fade'+ (f(this).scrollTop() > 2500 ? 'Out': 'In')](500);          
+
+    });
 
 });
-		
-close.addEventListener("click", function(event) {
-		
-	event.preventDefault();
-	popup.classList.remove("modal-content-show");
-		
-});
-		
-window.addEventListener("keydown", function(event) {
-    if (event.keyCode === 27) {
-        if (popup.classList.contains("modal-content-show")) {
-            popup.classList.remove("modal-content-show");
-        }
-    }
-});
-
-	  
-var mapOpen = document.querySelector(".js-open-map");
-
-var mapPopup = document.querySelector(".modal-content-map");
-		
-var mapClose = mapPopup.querySelector(".modal-content-close");
-		
-mapOpen.addEventListener("click", function(event) {
-event.preventDefault();
-mapPopup.classList.add("modal-content-show");
-});
-
-mapClose.addEventListener("click", function(event) {
-event.preventDefault();
-mapPopup.classList.remove("modal-content-show");
-});
-
-window.addEventListener("keydown", function(event) {
-	if (event.keyCode === 27) {
-		if (mapPopup.classList.contains("modal-content-show")) {
-            mapPopup.classList.remove("modal-content-show");
-        }
-    }
-});
-		
